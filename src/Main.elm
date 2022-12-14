6213a8b32f8c00b325c04019f3cc140d124ff5b5
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2022-12-14 10:37:40 +0100

    remove demo

--- src/Main.elm
+++ src/Main.elm
@@ -10,23 +10,20 @@ main =
 
 
 type alias Item =
-    { title : String, demo : String, source : String, blog : String }
+    { title : String, source : String, blog : String }
 
 
 items : List Item
 items =
     [ { title = "The language"
-      , demo = "/Blog1.elm"
       , source = "https://github.com/bouwe77/elm-blog-posts/blob/main/src/Blog1.elm"
       , blog = "https://bouwe.io/elm-baby-steps"
       }
     , { title = "Rendering HTML"
-      , demo = "/Blog2.elm"
       , source = "https://github.com/bouwe77/elm-blog-posts/blob/main/src/Blog2.elm"
       , blog = "https://bouwe.io/elm-rendering-html"
       }
     , { title = "User interaction"
-      , demo = "/Blog3.elm"
       , source = "https://github.com/bouwe77/elm-blog-posts/blob/main/src/Blog3.elm"
       , blog = "https://bouwe.io/elm-user-interaction"
       }
@@ -38,8 +35,7 @@ viewItem item =
     p []
         [ text item.title
         , ul []
-            [ li [] [ a [ href item.demo ] [ text "demo" ] ]
-            , li [] [ a [ href item.source ] [ text "source code" ] ]
+            [ li [] [ a [ href item.source ] [ text "source code" ] ]
             , li [] [ a [ href item.blog ] [ text "blog post" ] ]
             ]
         ]
