d951be57775e79001c1b2ff8646e69ed2234710a
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2023-03-11 20:26:24 +0100

    feat: hacky way of selecting which module to render

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import "./main.css";
-import { Elm } from "./Main.elm";
+import { Elm as blog1 } from "./Blog1.elm";
+import { Elm as blog2 } from "./Blog2.elm";
+import { Elm as blog3 } from "./Blog3.elm";
+import { Elm as blog4 } from "./Blog4.elm";
 import * as serviceWorker from "./serviceWorker";
 
-Elm.Main.init({
+// const blog = blog1.Blog1;
+// const blog = blog2.Blog2;
+// const blog = blog3.Blog3;
+const blog = blog4.Blog4;
+
+blog.init({
   node: document.getElementById("root"),
 });
 
